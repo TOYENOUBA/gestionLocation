d68f45d31cf6a201272fe957c0cb73a2512bba1e
TOYENOUBA <togyenoubarimtebaye@gmaol.com>
2022-03-11 09:15:23 +0100

    1

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use App\Models\Client;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(TypeArticleTableSeeder::class);
+        Article::factory(10)->create();
+        Client::factory(10)->create();
+        User::factory(10)->create();
+
+        $this->call(RoleTableSeeder::class);
+        $this->call(StatutLocationTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
+        $this->call(DureeLocationTableSeeder::class);
+
+        // \App\Models\User::factory(10)->create();
+    }
+}
